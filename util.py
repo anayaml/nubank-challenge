6528194de166cea2603885c146f32a955f0ce7b5
amslv <as.anasilva@outlook.com>
2023-05-18 16:22:58 -0300

    done

--- util.py
+++ util.py
@@ -1,4 +1,10 @@
 import json
 
 def gerar_arquivo_json(arquivo, valor):
-    with open("output\\"+ arquivo,'w+') as f:
+    resultado = {
+        "tax" : valor
+    }
+    json_output = json.dumps(resultado, indent=4)
+    with open("output\\"+ arquivo,'a+') as f:
+        f.write(json_output)
+        f.write(",")
